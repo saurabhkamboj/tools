dc9b709d086aa62f1312feb20ac9bf64bd01c9c3
Saurabh Kamboj <skamboj97@gmail.com>
2024-05-03 19:39:27 +0530

    Create folder and add file

---
+++ timer.rb
@@ -0,0 +1,17 @@
+def timer_with_beep(number)
+  puts "Timer started for #{number} minute#{'s' if number > 1}."
+  sleep(number * 60)
+
+  end_time = Time.now.strftime('%H:%M')
+  puts "Time's up at #{end_time}!"
+  `say "Time's up"`
+end
+
+def truncate_if_no_float(number)
+  number.to_i == number.to_f ? number.to_i : number.to_f
+end
+
+number = gets.chomp
+system 'clear'
+
+timer_with_beep(truncate_if_no_float(number))
